3dc90c457fddcb9b5e72e95464a38c5831e0bf77
tareimarcello <tredasto12@gmail.com>
2023-01-31 16:25:13 +0100

    First version

--- src/main/java/bean/BuyGadgetBean.java
+++ src/main/java/bean/BuyGadgetBean.java
@@ -2,12 +2,12 @@ package bean;
 /*Classe Bean coinvolta principalmente nel caso d'Uso Buy Ticket che però uso anche in questo caso
  */
 public class BuyGadgetBean {
-    private String ClubName;
-    public BuyGadgetBean(String Name){
-        this.ClubName=Name;
+    private String clubName;
+    public BuyGadgetBean(String name){
+        this.clubName=name;
     }
     public String getClubName(){
-        return this.ClubName;
+        return this.clubName;
     }
     private void CheckInput(){
     }
